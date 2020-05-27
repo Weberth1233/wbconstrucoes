cd99774965937ff0a6f6e47de593e0ad586611b9
Valter Anolar <Valter Anolar@DESKTOP-2PF7335>
2020-05-27 15:14:12 -0300

    Commit inícial.

---
+++ wbconstrucoes/src/br/unitins/wbconstrucoes/controller/WbConstrucoesController.java
@@ -0,0 +1,13 @@
+package br.unitins.wbconstrucoes.controller;
+
+import java.io.Serializable;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+@Named
+@ViewScoped
+public class WbConstrucoesController implements Serializable {
+	
+	private static final long serialVersionUID = 4229379253264816939L;
+	
+}
